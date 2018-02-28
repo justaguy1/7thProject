d6a397df0ad57a6749f46c505c919d8624bf9766
96bp19 <lanthoo170@gmail.com>
2018-02-28 10:22:24 +0545

    default Level created

---
+++ Seven/Source/Seven/HumanCharacter.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SevenCharacter.h"
+#include "HumanCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SEVEN_API AHumanCharacter : public ASevenCharacter
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
