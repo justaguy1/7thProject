362680245730fbd00f11dcbfb101fae1ceff709e
unknown <lanthoo170@gmail.com>
2018-04-04 18:53:53 +0545

    can switch weapon using shortcut key (left arrow here)

---
+++ Seven/Source/Seven/MyUserWidget.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MyUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SEVEN_API UMyUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+		
+	
+		
+	
+};
